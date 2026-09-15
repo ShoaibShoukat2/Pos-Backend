9f40ca19ae5c7fcfba4c9c3fa749d3f378aa1663
Shoaib <shoaibshoukatbhatti6252@gmail.com>
2026-09-15 14:22:08 +0500

    new update

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     origin.strip()
     for origin in os.getenv(
-        "CORS_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000"
+        "CORS_ORIGINS",
+        "http://localhost:3000,http://127.0.0.1:3000,https://pos-frontend-xi-navy.vercel.app"
     ).split(",")
     if origin.strip()
 ]
